79a020bd214afb461682ff69e8d51866f1165212
Connor812 <connorsavoy812@gmail.com>
2023-06-14 15:08:52 -0400

    added a sky and floor texture and a new file for coding the sprites

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        self.screen.fill('black')
+        # self.screen.fill('black')
         self.object_render.draw()
         # self.map.draw()
         # self.player.draw()
